6ab595338b11b58f176333bc6de3f8a8193c7d40
Marcin Chalecki <alcaster@op.pl>
2016-11-11 21:25:27 +0000

    Working celery

--- Thornhill/Thornhill/settings/dev.py
+++ Thornhill/Thornhill/settings/dev.py
@@ -11,3 +11,8 @@ CACHES = {
 }
 
 WSGI_APPLICATION = 'Thornhill.wsgi.application'
+
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
