cc5b7714d42edcc224d1236934936a24912c2639
Marcin Chalecki <alcaster0@gmail.com>
2017-01-03 19:42:49 +0100

    Clean up of unnecessary files(pycache,.idea/). Update .gitignore to follow these rules Deleted db. Db is in root folder now not outside.

--- Thornhill/Thornhill/settings/base.py
+++ Thornhill/Thornhill/settings/base.py
@@ -30,7 +30,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
